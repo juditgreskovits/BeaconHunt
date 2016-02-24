071e08b9b1bf7ef06db49334cd341b8d1df9a3d9
Joni Saksholm <joni@saksholm.fi>
2016-02-24 20:20:48 +0000

    menu button removed from top bar

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -14,12 +14,7 @@ Header = React.createClass({
           <div className="container">
 
             <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
+            
               <a className="navbar-brand" href="/">Treasure Hunt</a>
             </div>
 

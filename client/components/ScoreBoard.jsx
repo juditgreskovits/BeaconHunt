cabf2c2be191321fccec5a925979634a6f16b15d
Timothy Haerinck <timothy.haerinck@23seconds.be>
2016-02-25 02:40:53 +0000

    router scoreboard and code cleanage

--- client/components/ScoreBoard.jsx
+++ client/components/ScoreBoard.jsx
@@ -3,27 +3,27 @@ Scoreboard = React.createClass({
     mixins: [ReactMeteorData],
 
     getMeteorData() {
+        const gamesSubHandle = Meteor.subscribe("games");
+        const loading = !gamesSubHandle.ready();
+
+        let games = Games.find().fetch();
+        let gameWithHighestScore = Games.find().sort({score:-1}).limit(1);
         return {
-            games: () => {
-                const gamesSubHandle = Meteor.subscribe('games');
-                //const loading = !gamesSubHandle.ready();
+            games: this.gamesWithPercentages(games, gameWithHighestScore),
+            loading: loading
+        };
+    },
+    gamesWithPercentages(games, gamesWithPercentages) {
+        let divider = gameWithHighestScore / 100;
 
-                let gamesWithPercentages = [],
-                    gameWithHighestScore = Games.find().sort({score:-1}).limit(1),
-                    games = Games.find().fetch(),
-                    divider = gameWithHighestScore / 100;
-
-
-                for (let game of games) {
-                    gamesWithPercentages.push({
-                        name : game.name,
-                        score: game.score /divider //calculate score percentages
-                    });
-                }
-
-                return gamesWithPercentages;
-            }
+        for (let game of games) {
+            gamesWithPercentages.push({
+                name: game.name,
+                score: game.score / divider //calculate score percentages
+            });
         }
+
+        return gamesWithPercentages;
     },
     renderScoreboard() {
         return this.data.games.map((game) => {
@@ -31,6 +31,12 @@ Scoreboard = React.createClass({
         });
     },
     render() {
+        const loading = this.data.loading;
+
+        if(loading)
+        {
+            return <div>Loading...</div>
+        }
         return (
             <div>
                 {this.renderScoreboard()}

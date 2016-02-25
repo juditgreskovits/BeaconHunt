32a138f8439217f40cd04b2beb7098d98055bcd6
Timothy Haerinck <timothy.haerinck@23seconds.be>
2016-02-25 15:25:54 +0000

    working scoreboard

--- client/components/ScoreBoard.jsx
+++ client/components/ScoreBoard.jsx
@@ -9,7 +9,7 @@ Scoreboard = React.createClass({
 
 
         if(!loading) {
-            let games = Games.find().fetch();
+            let games = Games.find({}, {sort: {score: -1}}).fetch();
             let gameWithHighestScore = Games.find({}, {sort: {score: -1}}).fetch()[0];
             gamesWithPercentages = this.getGamesWithPercentages(games, gameWithHighestScore.score);
         }
@@ -33,9 +33,13 @@ Scoreboard = React.createClass({
         return gamesWithPercentages;
     },
     renderScoreboard() {
+
         return this.data.games.map((game) => {
-            return <div> {game.name} {game.score} </div>
-        });
+
+            //return <div> {game.name} {game.score} </div>
+            let myStyle={width:game.score +"%"};
+        return  <div className="scoreWrapper"><div className="scoreContents"><div className="scoreInner" style={myStyle}>{game.name} {game.score}</div></div></div>
+    });
     },
     render() {
         const loading = this.data.loading;
